fb875da1812418c6e907cabb21276da046a91043
timyang2005 <3072991924@qq.com>
2026-04-20 14:34:54 +0800

    Update build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Root build file for WebDAVSyncPlugin
+
+plugins {
+    id("com.android.application") version "9.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+}
