999e2909dfbcfbe2cc4ab0e869704f12adb5d29a
timyang2005 <3072991924@qq.com>
2026-04-20 14:41:20 +0800

    Fix Kotlin plugin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@
 
 plugins {
     id("com.android.application") version "9.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
 }
+
