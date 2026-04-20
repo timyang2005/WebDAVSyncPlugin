37983efba1489cb223d6cdc791b5d7d52885c8d4
timyang2005 <3072991924@qq.com>
2026-04-20 18:17:18 +0800

    test: minimal root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-// Root build file for WebDAVSyncPlugin
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.dynamic.feature) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
