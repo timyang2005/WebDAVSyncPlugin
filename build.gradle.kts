fcadcd6515437905ff93845b2487baeef68d477a
timyang2005 <3072991924@qq.com>
2026-04-20 15:06:15 +0800

    [CI] Fix plugin build: sync with template

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Root build file for WebDAVSyncPlugin
 
 plugins {
-    id("com.android.application") version "9.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.10" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.dynamic.feature) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.jetbrains.kotlin.jvm) apply false
 }
-
