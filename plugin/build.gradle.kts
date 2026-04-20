e23fb9f6159445cdb4199431b22f06140bfa0836
timyang2005 <3072991924@qq.com>
2026-04-20 20:14:12 +0800

    fix: use explicit version for kotlin.plugin.compose plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
 
 android {
