73de5f383c761fb052e8c1bf8880efdf7a3aa7a3
timyang2005 <3072991924@qq.com>
2026-04-20 13:29:43 +0800

    fix: Add maven.nariko.org/release repository for LNR API

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,24 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.nariko.org/release") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
